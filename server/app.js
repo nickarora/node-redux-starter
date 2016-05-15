d426d0b14ddf536ee7f202a6d986f0319b9a9c54
Nick Arora <arora.nick@gmail.com>
2016-05-14 21:32:25 -0700

    initialize express app

---
+++ server/app.js
@@ -0,0 +1,29 @@
+import Express from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
+import cors from 'cors'
+
+import routes from './routes'
+
+// import db from './config/db'
+
+const errorHandler = (error, req, res) => {
+  if (error.status && error.message) {
+    return res.status(error.status).send(error.message).end()
+  }
+
+  return res.status(500).send('Internal Server Error!').end()
+}
+
+const app = new Express()
+
+// Middleware
+app.use(cors())
+app.use(bodyParser.json({ type: ['application/json'] }))
+
+// Routes
+app.use('/api', routes)
+app.use('/', Express.static(path.resolve(__dirname, '../dist')))
+app.use(errorHandler)
+
+export default app
---
+++ server/server.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+import app from './app'
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.info('Server listening on port', PORT)
+})
+
+process.on('uncaughtException', err => {
+  console.error(err.stack || err)
+  console.warn('Exiting process because of uncaught exception.')
+  process.exit()
+})
+
+process.on('SIGTERM', err => {
+  console.warn('Received SIGTERM. Shutting down.', err)
+})

9bbf38ef55f6c986f95ecb59549aa6dd31c4b337
Nick Arora <arora.nick@gmail.com>
2016-06-09 17:35:04 -0700

    do basic server side validation on email address

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,15 +1,25 @@
 import { Router } from 'express'
 import { User } from '../models'
 
+import valid from '../util/validateEmail'
+
 const router = new Router()
 
 router.post('/signup', (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
 
+  if (!email || !password) {
+    res.status(422).send({ message: 'You must provide an email and password.' })
+  }
+
+  if (!valid(email)) {
+    res.status(422).send({ message: 'You must provide a valid email address.' })
+  }
+
   const signup = existingUser => {
     if (existingUser) {
-      res.status(422).send({ error: 'Email is already in use' })
+      res.status(422).send({ message: 'Email is already in use.' })
     }
 
     const newUser = new User({
---
+++ server/util/validateEmail.js
@@ -0,0 +1,7 @@
+/* eslint-disable max-len */
+const validateEmail = (email) => {
+  const emailValidationExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return emailValidationExp.test(email)
+}
+
+export default validateEmail

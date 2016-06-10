0f3711e6d5a257ce0d5cc89af90de03a5eb48682
Nick Arora <arora.nick@gmail.com>
2016-06-09 18:47:29 -0700

    encrypt the password before saving it to the database

--- server/models/schemas/user.js
+++ server/models/schemas/user.js
@@ -1,11 +1,26 @@
 import { Schema } from 'mongoose'
 import transform from '../util/transform'
+import Promise from 'bluebird'
+import bcryptNode from 'bcrypt-nodejs'
+
+const bcrypt = Promise.promisifyAll(bcryptNode)
+const SALT_ROUNDS = 10
 
 const UserSchema = new Schema({
   email: { type: String, unique: true, lowercase: true, required: true },
   password: { type: String, required: true },
 }, {
   timestamps: true,
+})
+
+UserSchema.pre('save', function(next) { // eslint-disable-line func-names
+  bcrypt.genSaltAsync(SALT_ROUNDS)
+    .then(salt => bcrypt.hashAsync(this.password, salt, null))
+    .then(hash => {
+      this.password = hash
+      next()
+    })
+    .catch(err => next(err))
 })
 
 export default transform(UserSchema)

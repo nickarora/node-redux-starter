b2e997c7a0864ca7799926a5987eb790e73c3a26
Nick Arora <arora.nick@gmail.com>
2016-07-24 18:37:39 -0700

    dont load styles when running tests

--- webpack/test.js
+++ webpack/test.js
@@ -1,8 +1,19 @@
+import PATHS from './paths'
+
 const test = {
   externals: {
     'react/addons': true,
     'react/lib/ExecutionEnvironment': true,
     'react/lib/ReactContext': true,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$|\.scss$|\.sass$/,
+        loader: 'null',
+        include: PATHS.src,
+      },
+    ],
   },
 }
 

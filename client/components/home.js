f11267c63187d2b5eaa945b0e0d2f25432f72392
Nick Arora <arora.nick@gmail.com>
2016-07-25 15:00:49 -0700

    add glyphicon to test font loading

--- client/components/home.js
+++ client/components/home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import { Col, Row } from 'react-bootstrap'
+import { Col, Row, Glyphicon } from 'react-bootstrap'
 
 const Home = () =>
   <Row>
     <Col xs={8}>
-      <h3>Home</h3>
+      <h3><Glyphicon glyph='home' /> Home</h3>
       <p className='welcome-message'>Welcome to the Node Redux Starter Kit</p>
     </Col>
   </Row>

ffa7c56b737c73940f7adf3b886b85f17bef78ae
Nick Arora <arora.nick@gmail.com>
2016-09-27 16:10:15 -0700

    rename pgTodo to Todo

--- server/routes/todos/index.js
+++ server/routes/todos/index.js
@@ -1,11 +1,10 @@
 import { Router } from 'express'
-// import { Todo } from '../../models'
-import pgTodo from '../../models/pgTodo'
+import Todo from '../../models/Todo'
 
 const router = new Router()
 
 router.get('/', (req, res, next) =>
-  pgTodo
+  Todo
     .findAll()
     .then(collection => collection.orderBy('-created_at').fetch())
     .then(ordered => res.status(200).json(ordered.serialize()))
@@ -13,21 +12,21 @@ router.get('/', (req, res, next) =>
 )
 
 router.put('/:id', (req, res, next) =>
-  pgTodo
+  Todo
     .update(req.body.todo, { id: req.params.id })
     .then(updatedTodo => res.status(200).json(updatedTodo.serialize()))
     .catch(err => next(err))
 )
 
 router.post('/', (req, res, next) => {
-  pgTodo
+  Todo
     .create(req.body.todo)
     .then(savedTodo => res.status(200).json(savedTodo.serialize()))
     .catch(err => next(err))
 })
 
 router.delete('/:id', (req, res, next) => {
-  pgTodo
+  Todo
     .destroy({ id: req.params.id })
     .then(_emptyTodo => res.status(200).json({ id: parseInt(req.params.id, 10) }))
     .catch(err => next(err))
